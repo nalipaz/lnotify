04cfe7f98deb3e0e913dab1ba5dea8bbbac785a3
Nicholas Alipaz <nicholas@alipaz.net>
2014-10-31 15:00:25 -0700

    Changing to use a remote client for notifications

--- lnotify.py
+++ lnotify.py
@@ -1,37 +1,19 @@
 #  Project: lnotify
-#  Description: A libnotify script for weechat. Uses
-#  subprocess.call to execute notify-send with arguments.
-#  Author: kevr <kevr@nixcode.us>
-#  License: GPL3
+#  Description: A libnotify script that sends notifications
+#  to a client through a socket.  The client does all the
+#  actual work and nofification in the remote system.
+#  Author: Nicholas Alipaz <nicholas@alipaz.net>
 #
-# 0.1.2
-# added option to display weechat's icon by tomboy64
-#
-# 0.1.3
-# changed the way that icon to WeeChat notification is specified.
-# (No absolute path is needed)
-# /usr/bin/notify-send isn't needed anymore.
-# (pynotify is handling notifications now)
-# changed the way that lnotify works. When using gnome 3, every new
-# notification was creating a new notification instance. The way that
-# it is now, all WeeChat notifications are in a group (that have the
-# WeeChat icon and have WeeChat name).
-# Got report that it has better look for KDE users too.
-#
-# 0.1.4
-# change hook_print callback argument type of displayed/highlight
-# (WeeChat >= 1.0)
-#
-# 0.2.0
-# - changed entire system to hook_process_hashtable calls to notify-send
-# - also changed the configuration option names and methods
-# Note: If you want pynotify, refer to the 'notify.py' weechat script
-import weechat as weechat, subprocess, string, os, urllib, urllib2, shlex
-from subprocess import Popen, PIPE
+import weechat as weechat, string, os, urllib, urllib2, shlex, zmq, json
 
 lnotify_name = "lnotify"
 lnotify_version = "0.2.0"
 lnotify_license = "GPL3"
+
+# Setup zmq
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5000")
 
 # convenient table checking for bools
 true = { "on": True, "off": False }
@@ -105,11 +87,14 @@ def handle_msg(data, pbuffer, date, tags, displayed, highlight, prefix, message)
             break
 
     buffer_name = weechat.buffer_get_string(pbuffer, "short_name")
-    if buffer_type == "private" and query:
-        notify_user("WeeChat: Private Message(k) from {} ({})".format(prefix, buffer_name), message, prefix, buffer_name)
+    if pbuffer == weechat.current_buffer() and buffer_type == "private" and query:
+        notify_user("WeeChat: Private Message from {}".format(prefix), message, prefix, buffer_name, "query")
+
+    elif buffer_type == "private" and query:
+        notify_user("WeeChat: Private Message from {}".format(prefix), message, prefix, buffer_name, "highlight")
 
     elif buffer_type == "channel" and highlight:
-        notify_user("WeeChat: Message(h) from {} ({})".format(prefix, buffer_name), message, prefix, buffer_name)
+        notify_user("WeeChat: Message from {} ({})".format(prefix, buffer_name), message, prefix, buffer_name, "highlight")
 
     return weechat.WEECHAT_RC_OK
 
@@ -119,38 +104,24 @@ def process_cb(data, command, return_code, out, err):
     elif return_code != 0:
         weechat.prnt("", "return_code = %d" % return_code)
         weechat.prnt("", "notify-send has an error")
+
     return weechat.WEECHAT_RC_OK
 
-def encrypt(text):
-    encryption_password = cfg["irssi_encryption_password"]
-    command="openssl enc -aes-128-cbc -salt -base64 -A -pass pass:%s" % (encryption_password)
-    output,errors = Popen(shlex.split(command),stdin=PIPE,stdout=PIPE,stderr=PIPE).communicate(text+" ")
-    output = string.replace(output,"/","_")
-    output = string.replace(output,"+","-")
-    output = string.replace(output,"=","")
-    return output
-
-def notify_user(origin, message, nick, channel):
-    hook = weechat.hook_process_hashtable("notify-send",
-        { "arg1": "-i", "arg2": cfg["icon"],
-          "arg3": "-a", "arg4": "WeeChat",
-          "arg5": origin, "arg6": message },
-        20000, "process_cb", "")
-
-    irsii_notifications = true[cfg["irssi_notifications"]]
-    if irsii_notifications:
-        irsii_api_token = cfg["irssi_api_token"]
-        url = "https://irssinotifier.appspot.com/API/Message"
-        postdata = urllib.urlencode({'apiToken':irsii_api_token,'nick':encrypt(nick),'channel':encrypt(channel),'message':encrypt(message),'version':13})
-        version = weechat.info_get("version_number", "") or 0
-        hook1 = weechat.hook_process_hashtable("url:"+url, { "postfields":  postdata}, 2000, "", "")
-
-    highlight_sound = cfg["highlight_sound"]
-    query_sound = cfg["query_sound"]
-    sounds = true[cfg["sounds"]]
-    sound_cmd = cfg["sound_cmd"]
-    if sounds:
-        do = subprocess.Popen([sound_cmd, highlight_sound], stderr=subprocess.STDOUT, stdout=subprocess.PIPE).communicate()[0]
+def notify_user(origin, message, nick, channel, query_type):
+    info = {
+        "origin": origin,
+        "message": message,
+        "nick": nick,
+        "channel": channel,
+        "query_type": query_type,
+        "highlight_sound": cfg["highlight_sound"],
+        "query_sound": cfg["query_sound"],
+        "sounds": true[cfg["sounds"]],
+        "sound_cmd": cfg["sound_cmd"],
+        "icon": cfg["icon"]
+    }
+    info = json.dumps(info)
+    socket.send("message " + info)
 
     return weechat.WEECHAT_RC_OK
 
@@ -161,4 +132,3 @@ if __name__ == "__main__":
 
     cfg = config()
     print_hook = weechat.hook_print("", "", "", 1, "handle_msg", "")
-
